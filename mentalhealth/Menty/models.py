21297377e6771b5e00f52c5796a45bd022a631d9
0901-herr <61222490+0901-herr@users.noreply.github.com>
2021-07-18 07:25:11 +0800

    Added html and css

--- mentalhealth/Menty/models.py
+++ mentalhealth/Menty/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+# Models are used to create database
+# Each class represents one database
+
+class Url(models.Model):
+    # two tables inside this database
+    link = models.CharField(max_length=1000)
+    uuid = models.CharField(max_length=10)
+
+
+# python manage.py makemigrations
+# python manage.py migrates
+# python manage.py createsuperuser
+# admin
+# admin@gmail.com
+# localhost/admin
