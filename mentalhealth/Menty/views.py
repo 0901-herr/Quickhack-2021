6502652983741033eca5a2993fdba0af147c708f
JunKang1118 <imjunkang@gmail.com>
2021-07-18 19:55:01 +0800

    delete function form views

--- mentalhealth/Menty/views.py
+++ mentalhealth/Menty/views.py
@@ -6,10 +6,3 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
-def create(request):
-    if request.method == 'POST':
-        new_name = "You"
-        new_score = 0
-        new_user = Leaderboard(name =new_name,score = new_score)
-        new_user.save()
-        return HttpResponse(new_name)
