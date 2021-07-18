71a26ac0e7573f00f9cc828bccfb9a85b97e91b0
0901-herr <61222490+0901-herr@users.noreply.github.com>
2021-07-18 13:09:49 +0800

    UI layout updated

--- mentalhealth/mentalhealth/urls.py
+++ mentalhealth/mentalhealth/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Menty.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+
+#+ static(settings.MEDIA_URL, document_root=settings.MEIDA_ROOT)
+
+# urlpatterns += staticfiles_urlpatterns()
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
