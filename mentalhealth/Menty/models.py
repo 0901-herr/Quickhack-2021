31968d9fb20a05f11881608c3cfcf13738e350c4
JunKang1118 <imjunkang@gmail.com>
2021-07-18 14:13:29 +0800

    l 1

--- mentalhealth/Menty/models.py
+++ mentalhealth/Menty/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 class leaderboard(models.Model):
-    title = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    score = models.CharField(max_length=100)
+    
 
     def __str__(self) -> str:
         return self.title
