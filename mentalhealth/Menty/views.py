cdf532e10cb7ec2ef5a5d8a0cd36b1ab0272d4e2
JunKang1118 <imjunkang@gmail.com>
2021-07-18 15:14:31 +0800

    Update views.py

--- mentalhealth/Menty/views.py
+++ mentalhealth/Menty/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Leaderboard
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def create(request):
+    if request.method == 'POST':
+        new_name = "You"
+        new_score = 0
+        new_user = Leaderboard(name =new_name,score = new_score)
+        new_user.save()
+        return HttpResponse(new_name)
