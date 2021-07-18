fe248613a2d1499306a9b80262bffefba71418ad
0901-herr <61222490+0901-herr@users.noreply.github.com>
2021-07-18 20:01:25 +0800

    some changes

--- mentalhealth/Menty/views.py
+++ mentalhealth/Menty/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+from .models import Task
+from .models import Leaderboard
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def create(request):
+    if request.method == 'POST':
+        your_leaderboard = Leaderboard(name="You", score="0 Tasks")
+        your_leaderboard.save()
+
+        # random_lst = []
+        # for i in range(3):
+        #     random_num = random.randrange(1, 11)
+        #     if random_num not in random_lst:
+        #         random_lst.append(random_num)
+
+        all_tasks = Task.objects.all()
+        tasks = all_tasks
+
+        # for task in all_tasks:
+        #     if int(task['task_id']) in random_lst:
+        #         tasks['task_id'] = task
+
+        print(tasks)
+
+        return HttpResponse({"tasks":tasks})
